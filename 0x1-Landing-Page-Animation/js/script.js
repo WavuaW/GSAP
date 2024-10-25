7c2accc6eaa4475b18efe41b5463b69dbe0c871f
WavuaW <wavuamuroka@gmail.com>
2024-10-25 12:07:47 +0300

    Sorted out the animation issues next I will be trying to make the text animate

--- 0x1-Landing-Page-Animation/js/script.js
+++ 0x1-Landing-Page-Animation/js/script.js
@@ -1,24 +1,51 @@
-let textWrapper = document.querySelector(".title");
+// let textWrapper = document.querySelector(".title");
 // textWrapper.innerHTML = textWrapper.textContent.replace(
-//     /\S/g,
-//     "<span class='letter'>&&</span>"
+//     /\&/g,
+//     "<span class='letter'>&</span>"
 // );
 
-const tl = gsap.timeline();
+// const tl = gsap.timeline();
 
-anime.timeline().add({
-    targets: ".title .letter",
-    translateY: [100, 0],
-    translateZ: 0,
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 2000,
-    delay: (el, i) => 4800 + 40 * i,
+// anime.timeline().add({
+//     targets: ".title .letter",
+//     translateY: [100, 0],
+//     translateZ: 0,
+//     opacity: [0, 1],
+//     ease: "power1.InOut",
+//     duration: 2000,
+//     delay: (el, i) => 4800 + 40 * i,
+// });
+
+function splitTextIntoSpans(selector) {
+    let elements = document.querySelectorAll(selector);
+    elements.forEach((element) => {
+      let text = element.innerText;
+      let splitText = text
+        .split("")
+        .map(function (char) {
+          return `<span>${char === " " ? "&nbsp;&nbsp;" : char}</span>`;
+        })
+        .join("");
+    element.innerHTML = splitText;
+    
+    });
+}
+
+splitTextIntoSpans(".header .title")
+
+
+gsap.to(".header .title span", {
+    y: 0,
+    stagger: 0.1,
+    duration: 2,
+    ease: "power4.inOut",
+    delay: 0.75,
 });
 
+
 gsap.to(".box", {
-    y: "-100%", 
-    ease: "power2.InOut", 
+    y: "-100vh", 
+    ease: "power2.inOut", 
     duration: 2.4,
     delay: 1,
 });
@@ -26,21 +53,21 @@ gsap.to(".box", {
 gsap.from("img", {
     scale: "2",
     duration: 4,
-    ease:"power2.InOut", 
+    ease:"power4.inOut", 
     delay: 0
 });
 
 gsap.to(".wrapper-img", {
     width: "400px",
     height: "500px",
-    ease: "power2.InOut",
+    ease: "power2.inOut",
     delay: 3.5
 });
 
 gsap.from(".img", {
     opacity: 0,
     duration: 0.4,
-    ease: "power2.InOut",
+    ease: "power2.inOut",
     delay: 3.4
 });
 
@@ -48,23 +75,23 @@ gsap.to(".left", {
     x: -400,
     rotation: -10,
     duration: 2,
-    ease: "power2.InOut",
+    ease: "power4.inOut",
     delay: 3.8
 });
 
 gsap.to(".right", {
-    x: 100,
+    x: 400,
     rotation: 10,
     duration: 2,
-    ease: "power2.InOut",
+    ease: "power4.inOut",
     delay: 3.8
 });
 
-gsap.from( "menu > div, .hero-container > div", {
+gsap.from( ".menu > div, .hero-container > div", {
         opacity: 0,
         y: 30,
         duration: 2,
-        ease: "expo.InOut",
+        ease: "power2.inOut",
         delay: 4.2,
         stagger: 0.1
     }
