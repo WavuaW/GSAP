0b43601af6c632bf81280c66d7b86c42f6e6c362
WavuaW <wavuamuroka@gmail.com>
2025-01-09 10:23:27 +0300

    split text function

--- 0x02-Scroll-Trigger/script.js
+++ 0x02-Scroll-Trigger/script.js
@@ -7,4 +7,18 @@ document.addEventListener("DOMContentLoaded", () => {
         lenis.raf(time * 750);
     });
     gsap.ticker.lagSmoothing(0);
+
+    function splitTextIntoSpans(selector) {
+        const elements = document.querySelectorAll
+        (selector);
+        elements.forEach((element) => {
+            const [firstDigit, secondDigit] = element.innerText;
+            element.innerHTML = `
+            <div class="digit-wrapper">
+                <span class="first">${firstDigit}</span>
+                <span class="second">${secondDigit}</span>
+            </div>
+            `;
+        });
+    }
 });
